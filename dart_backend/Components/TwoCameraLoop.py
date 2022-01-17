ac763f24e03ecdd858eee4f4c713be3baf583f40
Alexander-Robert-Keller <56433680+Alexander-Robert-Keller@users.noreply.github.com>
2022-01-17 23:02:11 +0100

    Add a dual camera main Loop

--- dart_backend/Components/TwoCameraLoop.py
+++ dart_backend/Components/TwoCameraLoop.py
@@ -9,17 +9,10 @@ def dual_camera_loop(websocket):
     videoStream_L, snapshot_cam_L = getVideoStream(src=0)
     videoStream_R, snapshot_cam_R = getVideoStream(src=1)
 
-    reference_image = snapshot_cam_R.copy()
+    reference_image_L = snapshot_cam_L.copy()
+    reference_image_R = snapshot_cam_R.copy()
+
     # cal_data_L, transformed_image_L, cal_data_R, transformed_image_R  = calibrateAll(snapshot_cam_L, snapshot_cam_R)
-
-    # # load reference image
-
-    # # calibration data
-    # calData_L = None
-    # draw_L = None
-    # calData_R = None
-    # draw_R = None
-
     # TODO: add new Calibration instead of loading
     calData_L, draw_L, calData_R, draw_R = readCalibrationData('calibrationData_L.pkl', 'calibrationData_R.pkl')
     websocket.CALIBRATION_DONE.set()
@@ -36,35 +29,59 @@ def dual_camera_loop(websocket):
         # reset reference image
         websocket.Global_LOCK.acquire()
         if websocket.IMAGE_COUNT == 0:
-            reference_image = snapshot_cam_L.copy()
+            reference_image_L = snapshot_cam_L.copy()
+            reference_image_R = snapshot_cam_R.copy()
             test_image_idx = 0
         websocket.Global_LOCK.release()
 
         # get new image and calculate dart tip
-        _, camRGB = videoStream_L.read()
-        snapshot_cam = camRGB.copy()
-        rectangle = snapshot_cam.copy()
-        mse, result, dart_contour_points = process_images(reference_image, snapshot_cam)
+        _L, camRGB_L = videoStream_L.read()
+        _R, camRGB_R = videoStream_L.read()
+
+        snapshot_cam_L = camRGB_L.copy()
+        snapshot_cam_R = camRGB_R.copy()
+
+        rectangle_L = snapshot_cam_L.copy()
+        rectangle_R = snapshot_cam_R.copy()
+
+        mse_L, result_L, dart_contour_points_L = process_images(reference_image_L, snapshot_cam_L)
+        mse_R, result_R, dart_contour_points_R = process_images(reference_image_R, snapshot_cam_R)
+
 
         # get dart tip value if image difference is high enough
-        if mse > 40:
+        if mse_L > 40 or mse_R > 40:
             # draw images
-            cv2.imshow("reference_image.jpg", reference_image)
-            cv2.rectangle(rectangle, dart_contour_points[0], dart_contour_points[1], (0, 0, 255), 2)
-            cv2.circle(rectangle, (result[0], result[1]), 3, (0, 255, 0), 2)
-            cv2.imshow("second", rectangle)
+            cv2.imshow("L - reference_image.jpg", reference_image_L)
+            cv2.rectangle("L -rect", rectangle_L, dart_contour_points_L[0], dart_contour_points_L[1], (0, 0, 255), 2)
+            cv2.circle(rectangle_L, (result_L[0], result_L[1]), 3, (0, 255, 0), 2)
+            cv2.imshow("L - second", rectangle_L)
+
+            cv2.imshow("R - reference_image.jpg", reference_image_R)
+            cv2.rectangle("R -rect", rectangle_R, dart_contour_points_R[0], dart_contour_points_R[1], (0, 0, 255), 2)
+            cv2.circle(rectangle_R, (result_R[0], result_R[1]), 3, (0, 255, 0), 2)
+            cv2.imshow("R - second", rectangle_R)
 
             # calc result value
-            new_dart_coord = showLatestDartLocationOnBoard(draw_L, result, calData_L)
-            game_point_result = detect_segment(new_dart_coord, calData_L)
-            print("Detected dart. The score is " + str(game_point_result) + " Points!")
+            new_dart_coord_L = showLatestDartLocationOnBoard(draw_L, result_L, calData_L)
+            new_dart_coord_R = showLatestDartLocationOnBoard(draw_R, result_R, calData_R)
+
+            game_point_result_L = detect_segment(new_dart_coord_L, calData_L)
+            game_point_result_R = detect_segment(new_dart_coord_R, calData_R)
+
+            print("Detected dart: ")
+            print("     Left  -> " + str(game_point_result_L))
+            print("     Right -> " + str(game_point_result_R))
+            chosen_score = choose_better_dart(game_point_result_L, game_point_result_R, dart_contour_points_L,
+                                              dart_contour_points_R)
+            print("     The chosen score is: " + str(chosen_score))
 
             # send result
-            websocket.send_changes({"request": 1, "value": game_point_result})
+            websocket.send_changes({"request": 1, "value": chosen_score})
 
             # increase count, replace reference image
             websocket.increase_image_count()
-            reference_image = snapshot_cam
+            reference_image_L = snapshot_cam_L
+            reference_image_R = snapshot_cam_R
 
         waitForKey()
 
@@ -73,7 +90,10 @@ def dual_camera_loop(websocket):
         if websocket.IMAGE_COUNT >= 3:
             websocket.Global_LOCK.release()
             websocket.ROUND_DONE.wait()
-            reference_image = snapshot_cam_L
+            _L, camRGB_L = videoStream_L.read()
+            _R, camRGB_R = videoStream_L.read()
+            snapshot_cam_L = camRGB_L.copy()
+            snapshot_cam_R = camRGB_R.copy()
             websocket.ROUND_DONE.clear()
         else:
             websocket.Global_LOCK.release()
